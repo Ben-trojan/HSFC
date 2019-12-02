2323abd9d19203f0da8455620d02b1d245e7e1bd
Ben Lewis <benlewis323@gmail.com>
2019-12-02 12:40:26 +0000

    initialized

---
+++ StackProject/StackProject/IntStack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackProject
+{
+    class IntStack
+    {
+        private int top ;
+        private int[] theStack = new int[10];
+
+        public void Push(int aStack)
+        {
+            theStack[top] = aStack;
+            
+            top++;
+        }
+
+        public string Pop()
+        {
+            top--;
+            return Convert.ToString(theStack[top]);
+        }
+        public int Depth()
+        {
+            return theStack[top];
+        }
+    } 
+}
---
+++ StackProject/StackProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StackProject
+{
+    class Program
+    {
+      public static void Main(string[] args)
+        {
+            IntStack myStack = new IntStack();
+
+            myStack.Push(5);
+            myStack.Push(4);
+            Console.WriteLine(myStack.Pop());
+            Console.ReadLine();
+            myStack.Push(8);
+            Console.WriteLine(myStack.Pop());
+            Console.ReadLine();
+            Console.WriteLine("depth is " + myStack.Depth());
+            Console.ReadLine();
+        }
+    }
+}
