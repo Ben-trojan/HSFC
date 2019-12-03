273d33045bef144205a732e7e47fbccc566ddce8
Ben Lewis <benlewis323@gmail.com>
2019-12-03 13:42:31 +0000

    initialized

---
+++ StackInteractiveProject/StackInteractiveProject/Stack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackInteractiveProject
+{
+    class Stack
+    {
+        private int[] theStack = new int[10];
+        private int top = 0;
+
+        public void Push(int aStack)
+        {
+            int stack = theStack[top];
+            top++;
+        }
+        public int Pop()
+        {
+            top--;
+            return theStack[top];
+            
+        }
+
+        public int depth()
+        {
+            return top;
+        }
+    }
+}
