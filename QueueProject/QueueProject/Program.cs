f8326f434299acf624ec63d2f3398736b5757254
Ben Lewis <benlewis323@gmail.com>
2019-12-16 12:39:28 +0000

    Created Program

---
+++ QueueProject/QueueProject/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace QueueProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! welcome to the queue!");
+            int menuinput;
+            QueueCreep myQueue = new QueueCreep();
+            myQueue.add(aName:);
+            myQueue.remove();
+            do
+            {
+                Console.WriteLine("please enter what you wish to do");
+                Console.WriteLine("press 1 to add an entity to the queue");
+                Console.WriteLine("press 2 to remove an entity to the queue");
+                Console.WriteLine("press 3 to give the queue's length");
+                Console.WriteLine("press 4 to display the queue");
+                Console.WriteLine("press 5 to quit");
+                menuinput = Convert.ToInt32(Console.ReadLine());
+
+                if (menuinput == 1)
+                {
+                    
+                }
+                if (menuinput == 2)
+                {
+
+                }
+                if (menuinput == 3)
+                {
+
+                }
+                if (menuinput == 4)
+                {
+
+                }
+
+            } while (menuinput != 5);
+            Console.WriteLine("thank you!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ QueueProject/QueueProject/QueueCreep.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueProject
+{
+    class QueueCreep
+    {
+        
+        string name;
+        
+
+
+        public void add (string aName)
+        {
+            name = aName;
+            Console.WriteLine("not implimented yet");
+        
+        }
+        public int remove()
+        {
+            Console.WriteLine("not implemented yet");
+            return 0;
+        }
+
+        public int length()
+        {
+            Console.WriteLine("not implemented");
+            return 0;
+        }
+    }   
+}
